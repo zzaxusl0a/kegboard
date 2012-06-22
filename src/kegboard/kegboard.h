a4f47cb99cbd88fc3ac48d0417a1a17f9e8ada34
Chris Niggel <zzaxusl0a@hotmail.com>
2012-06-22 07:58:36 -0700

    Revert "Revert "Revert "bringing inline w/ what is on the fridge"""
    This reverts commit 824cae56cdf9ee110d49bc70d01b0974e7e6fa86.

--- src/kegboard/kegboard.h
+++ src/kegboard/kegboard.h
@@ -67,11 +67,10 @@
 #define KB_METER_UPDATE_INTERVAL_MS 100
 
 // Number of relay outputs
-#define KB_NUM_RELAY_OUTPUTS 2
-
+#define KB_NUM_RELAY_OUTPUTS 6
 // Maximum time a relay will remain enabled after a "set_output" command.  The
 // timer is reset whenenver a new "set_output" command is received.
-#define KB_RELAY_WATCHDOG_MS 5000
+#define KB_RELAY_WATCHDOG_MS 10000
 
 // RFID defines
 #define STX 0x02
